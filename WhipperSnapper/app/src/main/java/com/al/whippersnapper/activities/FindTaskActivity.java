07cdf1999ca94ae0d606558d8788d73b4e666e52
Al Sweigart <asweigart@gmail.com>
2015-03-13 17:56:36 -0700

    Laying out groundwork for the Map/List slider for finding tasks.

--- WhipperSnapper/app/src/main/java/com/al/whippersnapper/activities/FindTaskActivity.java
+++ WhipperSnapper/app/src/main/java/com/al/whippersnapper/activities/FindTaskActivity.java
@@ -1,11 +1,15 @@
 package com.al.whippersnapper.activities;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.al.whippersnapper.R;
+import com.al.whippersnapper.adapters.CreateTaskFragmentPagerAdapter;
+import com.al.whippersnapper.adapters.FindTaskFragmentPagerAdapter;
+import com.astuetz.PagerSlidingTabStrip;
 
 public class FindTaskActivity extends ActionBarActivity {
 
@@ -13,6 +17,12 @@ public class FindTaskActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_task);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new FindTaskFragmentPagerAdapter(getSupportFragmentManager()));
+
+        PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        tabsStrip.setViewPager(viewPager);
     }
 
 
---
+++ WhipperSnapper/app/src/main/java/com/al/whippersnapper/adapters/FindTaskFragmentPagerAdapter.java
@@ -0,0 +1,45 @@
+package com.al.whippersnapper.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.al.whippersnapper.fragments.FindTaskListViewFragment;
+import com.al.whippersnapper.fragments.FindTaskMapViewFragment;
+import com.al.whippersnapper.fragments.TaskDetailsFragment;
+import com.al.whippersnapper.fragments.TaskLocationFragment;
+
+/**
+ * Created by Al on 3/13/2015.
+ */
+public class FindTaskFragmentPagerAdapter extends FragmentPagerAdapter {
+    public final int FRAGMENT_COUNT = 2;
+
+    public FindTaskFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+
+    @Override
+    public int getCount() {
+        return FRAGMENT_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: return FindTaskMapViewFragment.newInstance();
+            case 1: return FindTaskListViewFragment.newInstance();
+            default: return null; // this will cause an error
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0: return "Map";
+            case 1: return "List";
+            default: return null; // this will cause an error
+        }
+    }
+}
