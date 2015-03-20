c9396cb104fd9c715bc3074a8a0e00d45613dc57
Al Sweigart <asweigart@gmail.com>
2015-03-19 23:45:46 -0700

    Added the avatars to the user type picker activity.

--- WhipperSnapper/app/src/main/java/com/al/whippersnapper/activities/UserTypePickerActivity.java
+++ WhipperSnapper/app/src/main/java/com/al/whippersnapper/activities/UserTypePickerActivity.java
@@ -1,16 +1,22 @@
 package com.al.whippersnapper.activities;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Typeface;
+import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.al.whippersnapper.R;
+import com.al.whippersnapper.utils.GifAnimationDrawable;
+
+import java.io.IOException;
 
 public class UserTypePickerActivity extends ActionBarActivity {
     public static final int USER_TYPE_REQUEST_CODE = 42;
@@ -18,6 +24,12 @@ public class UserTypePickerActivity extends ActionBarActivity {
     private TextView tvAppTitle;
     private Button btnIAmASenior;
     private Button btnIAmAVolunteer;
+    private ImageView ivTitleSeniorAvatar1;
+    private ImageView ivTitleSeniorAvatar2;
+    private ImageView ivTitleVolunteerAvatar1;
+    private ImageView ivTitleVolunteerAvatar2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +45,29 @@ public class UserTypePickerActivity extends ActionBarActivity {
 
         btnIAmASenior.setTypeface(bikoTypeface);
         btnIAmAVolunteer.setTypeface(bikoTypeface);
+
+        // load the animated gifs of the little avatars
+        ivTitleSeniorAvatar1 = (ImageView) findViewById(R.id.ivTitleSeniorAvatar1);
+        ivTitleSeniorAvatar2 = (ImageView) findViewById(R.id.ivTitleSeniorAvatar2);
+        ivTitleVolunteerAvatar1 = (ImageView) findViewById(R.id.ivTitleVolunteerAvatar1);
+        ivTitleVolunteerAvatar2 = (ImageView) findViewById(R.id.ivTitleVolunteerAvatar2);
+        loadGifIntoImageView(ivTitleSeniorAvatar1, R.raw.title_senior_avatar1);
+        loadGifIntoImageView(ivTitleSeniorAvatar2, R.raw.title_senior_avatar2);
+        loadGifIntoImageView(ivTitleVolunteerAvatar1, R.raw.title_volunteer_avatar1);
+        loadGifIntoImageView(ivTitleVolunteerAvatar2, R.raw.title_volunteer_avatar2);
+    }
+
+    protected void loadGifIntoImageView(ImageView ivImage, int rawId) {
+        try {
+            GifAnimationDrawable anim = new GifAnimationDrawable(getResources().openRawResource(rawId));
+            ivImage.setImageDrawable(anim);
+            ((GifAnimationDrawable) ivImage.getDrawable()).setVisible(true, true);
+            anim.start();
+        } catch (Resources.NotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
